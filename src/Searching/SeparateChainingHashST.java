fd944649c011e790e48864b215f16e09f15e621c
xiaojun <1306410285@qq.com>
2018-03-28 23:23:32 +0800

    散列表

--- src/Searching/SeparateChainingHashST.java
+++ src/Searching/SeparateChainingHashST.java
@@ -1,5 +1,33 @@
 package Searching;
-
-public class SeparateChainingHashST {
+/*
+ * 基于拉链表的散列表
+ */
+public class SeparateChainingHashST<Key,Value> {
+	private int N;//键值总对数
+	private int M;//散列表的大小
+	private SequentialSearchST<Key, Value>[] st;//存放链表的对象
+	public SeparateChainingHashST() {
+		this(997);
+	}
+	public SeparateChainingHashST(int M) {
+		//创建M条链表
+		this.M=M;
+		st=(SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
+		for(int i=0;i<M;i++)
+			st[i]=new SequentialSearchST();
+	}
+	private int hash(Key key) {
+		return (key.hashCode()& 0x7fffffff)%M;
+	}
+	public Value get(Key key) {
+		return (Value) st[hash(key)].get(key);
+	}
+	public void put(Key key,Value val) {
+		st[hash(key)].put(key, val);
+	}
+	public Iterable<Key> keys(){
+		
+	}
+	
 
 }
